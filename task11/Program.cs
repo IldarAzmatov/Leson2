12f0184cd4b761fec8fad4023a8ffa512b505f6b
Ildar <ildar.azmatov@ya.ru>
2022-11-29 14:52:35 +0500

    удаление строки

---
+++ task11/Program.cs
@@ -0,0 +1,10 @@
+﻿//программа которая выводит случайное трех значное число и удаляет вторую цифру
+// 456-46/782- 72
+
+Random random = new Random();
+int number = random.Next(100, 1000);
+Console.WriteLine("Random nuber={0} ", number);
+int a = number /100;
+int c = number % 10;
+
+Console.WriteLine("Ответ" +a +c);
