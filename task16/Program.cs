4354cfc1ce7176b2edf0d23f1f92467e53a3a55e
Ildar <ildar.azmatov@ya.ru>
2022-11-29 18:30:02 +0500

    Квадрат числа

---
+++ task16/Program.cs
@@ -0,0 +1,11 @@
+﻿// евляется ли число квадратом другого числа.  Знак или  ||
+
+Console.WriteLine("Введите два числа ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b * b || b == a * a)
+    Console.WriteLine("Да");
+else
+{
+    Console.WriteLine("Нет");
+}
