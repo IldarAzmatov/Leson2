2e57a182c5fda5551537545affdc161eda32356a
Ildar <ildar.azmatov@ya.ru>
2022-11-29 15:53:00 +0500

    Задача

---
+++ task12/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите два числа ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a % b == 0)
+    Console.WriteLine("Кратно");
+else
+{
+    //Console.Write("Не кратно, остаток ");
+    Console.WriteLine($"Не кратно, остаток " + a % b);
+}
+
