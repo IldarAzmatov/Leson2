984dd6ef622b437d2efc9eaa30d17e03c568a270
Ildar <ildar.azmatov@ya.ru>
2022-11-30 12:12:05 +0500

    дни недели, выходной или нет

---
+++ dz15/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+    case 1:
+    Console.WriteLine("Нет");
+    break;
+    case 2:
+    Console.WriteLine("Нет");
+    break;
+    case 3:
+    Console.WriteLine("Нет");
+    break;
+    case 4:
+    Console.WriteLine("Нет");
+    break;
+    case 5:
+    Console.WriteLine("Нет");
+    break;
+    case 6:
+    Console.WriteLine("Да");
+    break;
+    case 7:
+    Console.WriteLine("Да");
+    break;
+    default:
+    Console.WriteLine("В неделе только 7 дней");
+    break;
+}
