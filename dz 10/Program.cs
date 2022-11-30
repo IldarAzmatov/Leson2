2bd939cc76b52ad41bc8693be519a75302998fe4
Ildar <ildar.azmatov@ya.ru>
2022-11-30 11:18:44 +0500

    Средняя цифра из трех значного числа

---
+++ dz 10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трех значное число ");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+        {
+            Console.WriteLine("{0}->{1}",input, input[1]);
+            break;
+        }
+    }
