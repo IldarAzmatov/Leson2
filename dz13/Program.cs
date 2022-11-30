3edb51ceb3ef990793430bff8d28a45c401c8e6b
Ildar <ildar.azmatov@ya.ru>
2022-11-30 12:04:20 +0500

    Вывод третьей цифры из любой длины чисел

---
+++ dz13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+string n = Console.ReadLine();
+int i = 0;
+if (Convert.ToInt32(n) <= 99)
+    Console.WriteLine("Третьей цифры нет");
+else
+{
+     char[] array = new char[n.Length];
+    
+        while(i < n.Length)
+        {
+            array[i] = n[i];
+            i ++;
+        }
+    Console.WriteLine("Третья цифра = " + array[2]);
+}
